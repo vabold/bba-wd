cf8adad44dcc0bc91476fb9ade1c9d2841e32fe1
Aiden <86704247+vabold@users.noreply.github.com>
2023-12-14 07:40:09 -0500

    Decompile EGG::Thread

--- include/sdk/OSThread.h
+++ include/sdk/OSThread.h
@@ -8,9 +8,19 @@ extern "C" {
 #endif
 
 typedef struct {
+    u8 _000[0x304 - 0x000];
+    u8 *stackBase;
+    u32 *stackEnd;
+    u8 _30c[0x318 - 0x30c];
 } OSThread;
 
+void *OSSetSwitchThreadCallback(void *callback);
 OSThread *OSGetCurrentThread();
+BOOL OSIsThreadTerminated(OSThread *thread);
+BOOL OSCreateThread(OSThread *thread, void *callback, void *param, void *stack, u32 stackSize,
+        s32 priority, u16 attr);
+void OSCancelThread(OSThread *thread);
+void OSDetachThread(OSThread *thread);
 
 #ifdef __cplusplus
 }
