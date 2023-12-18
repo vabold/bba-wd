7692a3c9491d8882201055d1fa4a191c63652fd3
Elijah Thomas <42302100+elijah-thomas774@users.noreply.github.com>
2023-12-17 19:43:38 -0500

    EGG: Implement Quatf (#20)
    * eggQuat split+linked
    
    * Formatting
    
    * Made Suggested Changes

---
+++ include/egg/math/eggQuat.h
@@ -0,0 +1,44 @@
+#include <egg/math/eggVector.h>
+
+namespace EGG {
+
+struct Quatf {
+    Quatf() {}
+    Quatf(f32 f, Vector3f v) : w(f), v(v) {}
+
+    ~Quatf() {}
+
+    friend Quatf operator*(const Quatf &, const Vector3f &);
+
+    // TODO: Implement
+    friend Quatf operator*(const Quatf &, const Quatf &) {
+        return Quatf();
+    };
+
+    void set(f32 fw, f32 fx, f32 fy, f32 fz);
+    void setRPY(const EGG::Vector3f &rpy);
+    Vector3f calcRPY();
+    void setRPY(f32 roll, f32 pitch, f32 yaw);
+    void setAxisRotation(const Vector3f &axis, f32 rot);
+    f32 norm();
+    void normalise();
+    Quatf conjugate();
+    Vector3f rotateVector(const Vector3f &vec);
+    void slerpTo(const Quatf &q2, f32 t, Quatf &out) const;
+    void makeVectorRotation(Vector3f &from, Vector3f &to);
+
+    void multScalar(f32 s) {
+        w *= s;
+        v.x *= s;
+        v.y *= s;
+        v.z *= s;
+    }
+    void setUnit() {
+        set(1.0f, 0.0f, 0.0f, 0.0f);
+    }
+
+    Vector3f v;
+    f32 w;
+};
+
+} // namespace EGG
---
+++ src/egg/math/eggQuat.cpp
@@ -0,0 +1,171 @@
+#include <egg/math/eggQuat.h>
+
+#pragma exceptions on
+
+namespace EGG {
+
+void Quatf::set(f32 fw, f32 fx, f32 fy, f32 fz) {
+    w = fw;
+    v.x = fx;
+    v.y = fy;
+    v.z = fz;
+}
+
+void Quatf::setRPY(const EGG::Vector3f &rpy) {
+    const f32 cy = Math<f32>::cos(rpy.z * 0.5f);
+    const f32 cp = Math<f32>::cos(rpy.y * 0.5f);
+    const f32 cr = Math<f32>::cos(rpy.x * 0.5f);
+    const f32 sy = Math<f32>::sin(rpy.z * 0.5f);
+    const f32 sp = Math<f32>::sin(rpy.y * 0.5f);
+    const f32 sr = Math<f32>::sin(rpy.x * 0.5f);
+
+    const f32 cy_cp = cy * cp;
+    const f32 sy_sp = sy * sp;
+    const f32 cy_sp = cy * sp;
+    const f32 sy_cp = sy * cp;
+
+    w = (cy_cp * cr) + (sy_sp * sr);
+    v.x = (cy_cp * sr) - (sy_sp * cr);
+    v.y = (cy_sp * cr) + (sy_cp * sr);
+    v.z = (sy_cp * cr) - (cy_sp * sr);
+}
+
+Vector3f Quatf::calcRPY() {
+    Vector3f rpy;
+
+    const f32 ww = w * w;
+    const f32 yy = v.y * v.y;
+    const f32 xx = v.x * v.x;
+    const f32 zz = v.z * v.z;
+
+    const f32 a0 = (ww + xx) - yy - zz;
+    const f32 a1 = (v.x * v.y + w * v.z) * 2.0f;
+    const f32 a2 = (v.x * v.z - w * v.y) * 2.0f;
+    const f32 a3 = (v.y * v.z + w * v.x) * 2.0f;
+    const f32 a4 = (ww - xx) - yy + zz;
+
+    const f32 a5 = Math<f32>::abs(a2);
+
+    if (a5 > 0.999999f) {
+        f32 t0 = (v.x * v.y - w * v.z) * 2.0f;
+        f32 t1 = (v.x * v.z + w * v.y) * 2.0f;
+
+        rpy.x = 0.0f;
+        rpy.y = a2 / a5 * -Math<f32>::pi_half();
+        rpy.z = Math<f32>::atan2(-t0, -a2 * t1);
+    } else {
+        rpy.x = Math<f32>::atan2(a3, a4);
+        rpy.y = Math<f32>::asin(-a2);
+        rpy.z = Math<f32>::atan2(a1, a0);
+    }
+
+    return rpy;
+}
+
+void Quatf::setRPY(f32 roll, f32 pitch, f32 yaw) {
+    setRPY(Vector3f(roll, pitch, yaw));
+}
+
+void Quatf::setAxisRotation(const Vector3f &axis, f32 rot) {
+    const f32 half_angle = rot * 0.5f;
+    const f32 cos = Math<f32>::cos(half_angle);
+    const f32 sin = Math<f32>::sin(half_angle);
+    set(cos, sin * axis.x, sin * axis.y, sin * axis.z);
+}
+
+// Found in SS, not BBA(at least with these other funcs)
+// f32 Quatf::norm() {
+//     return w * w + v.dot(v);
+// }
+// void Quatf::normalise() {
+//     f32 mag = Math<f32>::sqrt(norm());
+//     if (mag > 0.0f) {
+//         multScalar(Math<f32>::inv(mag));
+//     }
+// }
+
+Quatf Quatf::conjugate() {
+    // force sdata2 odering. Normalise introduces it through Math<f32>::inv
+    1.0f;
+
+    Quatf q;
+    q.w = w;
+    q.v = -1.0f * v;
+    return q;
+}
+
+Vector3f Quatf::rotateVector(const Vector3f &vec) {
+    Quatf conj, mult;
+    conj = conjugate();
+    mult = *this * vec;
+    mult = mult * conj;
+    return mult.v;
+}
+
+Quatf operator*(const Quatf &q, const Vector3f &v) {
+    Vector3f crossed = q.v.cross(v);
+    Vector3f scaled = q.w * v;
+    Quatf ret = Quatf(-q.v.dot(v), crossed + scaled);
+    return ret;
+}
+
+void Quatf::slerpTo(const Quatf &q2, f32 t, Quatf &out) const {
+    f32 dot = v.x * q2.v.x + v.y * q2.v.y + v.z * q2.v.z + w * q2.w;
+
+    if (dot > 1.0f) {
+        dot = 1.0f;
+    } else if (dot < -1.0f) {
+        dot = -1.0f;
+    }
+
+    bool lt0;
+    if (dot < 0.0) {
+        dot = -dot;
+        lt0 = true;
+    } else {
+        lt0 = false;
+    }
+
+    const f32 theta_0 = Math<f32>::acos(dot);
+    const f32 sin_theta_0 = Math<f32>::sin(theta_0);
+
+    f32 a, b;
+    if (Math<f32>::abs(sin_theta_0) < 1e-5f) {
+        a = 1.0f - t;
+        b = t;
+    } else {
+        const f32 sin_theta_0_inv = 1.0f / sin_theta_0;
+        const f32 theta = t * theta_0;
+        a = sin_theta_0_inv * Math<f32>::sin(theta_0 - theta);
+        b = sin_theta_0_inv * Math<f32>::sin(theta);
+    }
+
+    if (lt0) {
+        b = -b;
+    }
+
+    out.v.x = a * v.x + b * q2.v.x;
+    out.v.y = a * v.y + b * q2.v.y;
+    out.v.z = a * v.z + b * q2.v.z;
+    out.w = a * w + b * q2.w;
+}
+
+void Quatf::makeVectorRotation(Vector3f &from, Vector3f &to) {
+    Vector3f cross = from.cross(to);
+    f32 t0 = (from.dot(to) + 1) * 2.0f;
+
+    if (t0 < 0.0f) {
+        t0 = 0.0f;
+    }
+
+    t0 = Math<f32>::sqrt(t0);
+
+    if (t0 <= Math<f32>::epsilon()) {
+        setUnit();
+    } else {
+        const f32 inv = 1.0f / t0;
+        set(t0 * 0.5f, cross.x * inv, cross.y * inv, cross.z * inv);
+    }
+}
+
+} // namespace EGG
