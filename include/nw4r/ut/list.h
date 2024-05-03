0c2cd23d680ddbd3bf163f239c04dfde680b2681
Aiden <86704247+vabold@users.noreply.github.com>
2024-05-03 06:18:53 -0400

    Decompile nw4r::ut::List

--- include/nw4r/ut/list.h
+++ include/nw4r/ut/list.h
@@ -19,11 +19,42 @@ struct Link {
 
 void List_Init(List *list, u16 offset);
 void List_Append(List *list, void *object);
+void List_Insert(List *list, void *target, void *object);
 void List_Remove(List *list, void *object);
 void *List_GetNext(const List *list, const void *object);
+void *List_GetPrev(const List *list, const void *object);
+void *List_GetNth(const List *list, u16 n);
 
-inline void *List_GetFirst(List *list) {
+inline const void *List_GetNextConst(const List *list, const void *object) {
+    return List_GetNext(list, object);
+}
+
+inline const void *List_GetPrevConst(const List *list, const void *object) {
+    return List_GetPrev(list, object);
+}
+
+inline const void *List_GetNthConst(const List *list, u16 n) {
+    return List_GetNth(list, n);
+}
+
+inline void *List_GetFirst(const List *list) {
     return List_GetNext(list, NULL);
+}
+
+inline const void *List_GetFirstConst(const List *list) {
+    return List_GetNextConst(list, NULL);
+}
+
+inline void *List_GetLast(const List *list) {
+    return List_GetPrev(list, NULL);
+}
+
+inline const void *List_GetLastConst(const List *list) {
+    return List_GetPrevConst(list, NULL);
+}
+
+inline u16 List_GetSize(const List *list) {
+    return list->numObjects;
 }
 
 } // namespace ut
