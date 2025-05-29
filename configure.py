91d3e1f94d277e93b4e37f9ad778d514dd3b62da
Aiden <vabold820@gmail.com>
2025-05-29 16:04:02 -0400

    Split EGG::LongStopWatch

--- configure.py
+++ configure.py
@@ -291,6 +291,7 @@ config.libs = [
             Object(Matching, "egg/core/eggScene.cpp"),
             Object(Matching, "egg/core/eggDisposer.cpp"),
             Object(Matching, "egg/core/eggArchive.cpp"),
+            Object(NonMatching, "egg/core/eggLongStopWatch.cpp"),
         ],
     },
     {
